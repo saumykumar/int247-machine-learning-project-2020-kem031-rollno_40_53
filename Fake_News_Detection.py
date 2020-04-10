66e1f0a6d8c6b7da23afc83cc94d243cd55d1cf0
Arun1717 <49493567+Arun1717@users.noreply.github.com>
2020-04-10 19:50:22 +0530

    Update Fake_News_Detection.py

--- Fake_News_Detection.py
+++ Fake_News_Detection.py
@@ -7,3 +7,16 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+df=pd.read_csv('C:\\Users\\LENOVO\\Downloads\\news.csv')
+#Get shape and head
+df.shape
+df.head()
+#Get the labels
+labels=df.label
+labels.head()
+#Split the dataset
+x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
+#Initialize a TfidfVectorizer.For the stopping word
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
+Classifier=[PassiveAggressiveClassifier,DecisionTreeClassifier,RandomForestClassifier,LinearSVC,LogisticRegression]
+Name=['PassiveAggressiveClassifier','DecisionTreeClassifier','RandomForestClassifier','LinearSVC','LogisticRegression']
